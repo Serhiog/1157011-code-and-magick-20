75a9a94ca451b7da4b349b7634ca904e5cebf19f
Сергей Грищенков <grishenkov.sergei@gmail.com>
2020-07-06 16:06:59 -0500

    Сделал задание с сетью учебного проекта

--- js/backend.js
+++ js/backend.js
@@ -1,27 +1,61 @@
 'use strict';
 (function () {
 
-  var URL = 'https://javascript.pages.academy/code-and-magick/data';
+  var GETURL = 'https://javascript.pages.academy/code-and-magick/data';
+  var POSTURL = 'https://javascript.pages.academy/code-and-magick';
+  var successCode = 200;
 
   window.backend = {
 
-    backEndLoad: function (onLoad, onError) {
+    load: function (onLoad, onError) {
       var xhr = new XMLHttpRequest();
       xhr.addEventListener('load', function () {
-        if (xhr.status === '200') {
+        if (xhr.status === successCode) {
           onLoad(xhr.response);
         } else {
           onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
         }
       });
+
       xhr.responseType = 'json';
-      xhr.open('GET', URL);
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = 10000;
+      xhr.open('GET', GETURL);
       xhr.send();
     },
-    backEndSave: function (data, onLoad, onError) { }
 
+    save: function (data, onLoadForm, onError) {
+      var xhr = new XMLHttpRequest();
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === successCode) {
+          onLoadForm();
+        } else {
+          onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = 10000;
+      xhr.open('POST', POSTURL);
+      xhr.send(data);
+    }
   };
-  window.backend.backEndLoad();
-
-
 })();
